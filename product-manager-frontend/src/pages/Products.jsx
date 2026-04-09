b143feb4cefb829ddd9c66bfd962d2286ddf3856
AgashJeeva <agashbilla2001@gmail.com>
2026-04-09 12:42:52 +0530

    feat: implement product fetching and display in Products component

--- product-manager-frontend/src/pages/Products.jsx
+++ product-manager-frontend/src/pages/Products.jsx
@@ -1,9 +1,49 @@
+import { useEffect, useState } from "react";
+import API from "../services/api";
+
 function Products() {
-    return (
-        <div>
-            <h2>Products Dashboard</h2>
-        </div>
-    )
+  const [products, setProducts] = useState([]);
+
+  //FETCH PRODUCTS
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await API.get("/products");
+        setProducts(res.data);
+      } catch (err) {
+        console.error(err);
+        alert("Failed to load products");
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <h2>Products Dashboard</h2>
+
+      <table border="1" cellPadding="10">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {products.map((p) => (
+            <tr key={p._id}>
+              <td>{p.name}</td>
+              <td>{p.price}</td>
+              <td>{p.description}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Products;
